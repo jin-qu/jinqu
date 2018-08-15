5f1ed3bd75df754ead637f22f2195fc7790c17ff
Umut Özel <umutozel@outlook.com>
2018-08-15 14:14:14 +0300

    Improve

--- lib/query-part.ts
+++ lib/query-part.ts
@@ -1,4 +1,4 @@
-import { tokenize, Expression } from 'jokenizer';
+import { tokenize, Expression, evaluate } from 'jokenizer';
 
 export interface IQueryPart {
     readonly type: string;
@@ -11,6 +11,8 @@ export interface IQueryPart {
 export class QueryPart implements IQueryPart {
 
     constructor(private _type: string, private _func: Function, private _expStr: string, private _scopes: any[] = []) {
+        if (!_type) throw new Error('Type of QueryPart cannot be null or empty.');
+        if (!_func && !_expStr) throw new Error('Function or expression string must be provided.');
     }
 
     get type() {
@@ -20,31 +22,87 @@ export class QueryPart implements IQueryPart {
     get func() {
         if (this._func != null) return this._func;
 
-        const e = this.exp;
-        if (e == null) throw new Error('Expression cannot be null');
-
-        return this._func = (scopes: []) => evaluate(e, scopes);
+        return this._func = (scopes: []) => evaluate(this.exp, scopes);
     }
 
     get expStr() {
         if (this._expStr != null) return this._expStr;
 
-        const f = this.func;
-        if (f == null) throw new Error('Function cannot be null');
-
-        return this._expStr = f.toString();
+        return this._expStr = this.func.toString();
     }
 
+    private _exp: Expression;
     get exp() {
         if (this._exp != null) return this._exp;
 
-        const s = this.expStr;
-        if (s == null) throw new Error('Expression string cannot be null')
-
-        return this._exp = tokenize(s);
+        return this._exp = tokenize(this.expStr);
     }
 
     get scopes() {
         return this._scopes;
     }
+
+    static where<T>(predicate: (i: T) => boolean | string, ...scopes) {
+        const args = handleArgs(predicate);
+        return new QueryPart(QueryPartType.where, args[0], args[1], scopes);
+    }
+
+    static ofType<TResult>(type: new(...args) => TResult) {
+        return new QueryPart(QueryPartType.ofType, type, null);
+    }
+
+    static cast<TResult>(type: new(...args) => TResult) {
+        return new QueryPart(QueryPartType.cast, type, null);
+    }
 }
+
+function handleArgs(arg) {
+    return arg && typeof arg === "function"
+        ? [arg, null]
+        : [null, arg];
+}
+
+export const QueryPartType = {
+    where: 'where', 
+    ofType: 'ofType',
+    cast: 'cast',
+    select: 'select',
+    selectMany: 'selectMany',
+    join: 'join',
+    groupJoin: 'groupJoin',
+    orderBy: 'orderBy',
+    orderByDescending: 'orderByDescending',
+    thenBy: 'thenBy',
+    thenByDescending: 'thenByDescending',
+    take: 'take',
+    takeWhile: 'takeWhile',
+    skip: 'skip',
+    skipWhile: 'skipWhile',
+    groupBy: 'groupBy',
+    distinct: 'distinct',
+    concat: 'concat',
+    zip: 'zip',
+    union: 'union',
+    intersect: 'intersect',
+    except: 'except',
+    first: 'first',
+    firstOrDefault: 'firstOrDefault',
+    last: 'last',
+    lastOrDefault: 'lastOrDefault',
+    single: 'single',
+    singleOrDefault: 'singleOrDefault',
+    elementAt: 'elementAt',
+    elementAtOrDefault: 'elementAtOrDefault',
+    defaultIfEmpty: 'defaultIfEmpty',
+    contains: 'contains',
+    reverse: 'reverse',
+    sequenceEqual: 'sequenceEqual',
+    any: 'any',
+    all: 'all',
+    count: 'count',
+    min: 'min',
+    max: 'max',
+    sum: 'sum',
+    average: 'average',
+    aggregate: 'aggregate'
+};
