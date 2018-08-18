315d9e4941be6c19325905c1ace8abe529da654d
Umut Özel <umutozel@outlook.com>
2018-08-18 23:05:42 +0300

    Add repeat and range static methods to array

--- lib/array-extensions.ts
+++ lib/array-extensions.ts
@@ -9,14 +9,14 @@ declare global {
     }
 }
 
-Array.prototype.asQueryable = function() {
+Array.prototype.asQueryable = function () {
     return new ArrayQueryProvider(this).createQuery();
 }
 Array.prototype.q = Array.prototype.asQueryable;
 
 function extendArray(func: string) {
     if (Array.prototype[func]) return;
-    
+
     Array.prototype[func] = function () {
         const q = this.asQueryable();
         return q[func].apply(q, arguments);
@@ -24,3 +24,40 @@ function extendArray(func: string) {
 }
 
 Object.getOwnPropertyNames(QueryFunc).forEach(extendArray);
+
+declare global {
+    interface ArrayConstructor {
+        range(start: number, count: number);
+        repeat(item: any, count: number);
+    }
+}
+
+if (!Array.hasOwnProperty("range")) {
+    Array.range = function*(start, count) {
+        if (arguments.length == 0) return [];
+        if (arguments.length == 1) {
+            count = start;
+            start = 0;
+        }
+        if (count < 0)
+            throw new Error('Specified argument was out of the range of valid values');
+
+        for (var i = 0; i < count; i++) 
+            yield start + i;
+    };
+}
+
+if (!Array.hasOwnProperty("repeat")) {
+    Array.repeat = function*(item, count) {
+        if (arguments.length == 0) return [];
+        if (arguments.length == 1) {
+            count = item;
+            item = null;
+        }
+        if (count < 0)
+            throw new Error('Specified argument was out of the range of valid values');
+
+        for (var i = 0; i < count; i++)
+            yield item;
+    };
+}
