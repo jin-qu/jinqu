2cdca99224b8d78175ed811c25af378b6e30ca0f
Umut Özel <umutozel@outlook.com>
2018-08-22 12:39:27 +0300

    improve coverage

--- test/array-extension.spec.ts
+++ test/array-extension.spec.ts
@@ -8,10 +8,13 @@ describe('Array extension tests', () => {
     it('should create range', () => {
         expect(Array.from(Array.range(1, 5))).to.deep.equal([1, 2, 3, 4, 5]);
         expect(Array.from(Array.range(5))).to.deep.equal([0, 1, 2, 3, 4]);
+        expect(Array.range(1, -1)).to.throw;
+        expect(Array.range(-1)).to.throw;
     });
 
     it('should should repeat given item', () => {
         expect(Array.from(Array.repeat('JS', 3))).to.deep.equal(['JS', 'JS', 'JS']);
+        expect(Array.repeat('JS', -1)).to.throw;
     });
 
     it('should create all query functions on Array', () => {
@@ -19,5 +22,12 @@ describe('Array extension tests', () => {
         const haveItAll = Object.getOwnPropertyNames(QueryFunc).every(f => f in arr);
 
         expect(haveItAll).to.be.true;
+    });
+    
+    it('should work for a sample function', () => {
+        const arr = [1, 2, 3, 4, 5];
+        const query = arr.q();
+
+        expect(query.where(i => i > 3).count()).to.equal(2);
     });
 });
