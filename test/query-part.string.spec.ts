9742136a9f795b22eb7fe07e7648a6456c9a82f3
Umut Özel <umutozel@outlook.com>
2018-09-26 10:47:41 +0300

    refactor - order tests by name

--- test/query-part.string.spec.ts
+++ test/query-part.string.spec.ts
@@ -3,83 +3,42 @@ import 'mocha';
 import { Order, orders, products } from './fixture';
 import '..';
 
-describe('Query part tests with strings', () => {
+describe('Jinqu should be able to use string expressions with', () => {
 
-    it('should filter the array', () => {
-        const result = orders.asQueryable().where('c => c.id > 3').toArray();
-        expect(result).property('length').to.equal(2);
-        expect(result[0].id).to.equal(4);
-        expect(result[1].no).to.equal('Ord5');
+    it('aggregate', () => {
+        expect([1, 2, 3, 4].asQueryable().aggregate('(seed, value) => seed + value')).to.equal(10);
+        expect([1, 2, 3, 4].asQueryable().aggregate('(seed, value) => seed + value', 32)).to.equal(42);
+
+        const agg = orders.asQueryable().aggregate('(seed, order) => seed + order.id', 69, 'v => v / 2');
+        expect(agg).to.equal(42);
     });
 
-    it('should select only given members', () => {
-        const ids = orders.asQueryable().select(o => o.id).toArray();
-        expect(ids).to.deep.equal([1, 2, 3, 4, 5]);
+    it('all', () => {
+        expect([1, 2, 3, 4].asQueryable().all('i => i > 0')).to.be.true;
+        expect([1, 2, 3, 4].asQueryable().any('i => i > 4')).to.be.false;
 
-        const idNo = orders.asQueryable().select('o => ({ id: o.id, no: o.no })').toArray();
-        expect(idNo).to.deep.equal([
-            { id: 1, no: 'Ord1' },
-            { id: 2, no: 'Ord2' },
-            { id: 3, no: 'Ord3' },
-            { id: 4, no: 'Ord4' },
-            { id: 5, no: 'Ord5' }
-        ]);
+        expect(products.asQueryable().all('p => p.category !== null')).to.be.true;
+        expect(products.asQueryable().all('p => p.name === "None"')).to.be.false;
     });
 
-    it('should select all details to one array', () => {
-        const details = orders.asQueryable().selectMany('o => o.details').toArray();
-        expect(details.length).to.equal(16);
+    it('any', () => {
+        expect([1, 2, 3, 4].asQueryable().any('i => i > 3')).to.be.true;
+        expect([1, 2, 3, 4].asQueryable().any('i => i > 4')).to.be.false;
+
+        const p5 = products[5];
+        expect(products.asQueryable().any('p => p.name === p5.name', { p5 })).to.be.true;
+        expect(products.asQueryable().any('p => p.name === "None"')).to.be.false;
     });
 
-    it('should sort order details', () => {
-        const sortedDetails = orders[4].details.asQueryable()
-            .orderBy('d => d.supplier')
-            .thenByDescending('d => d.count')
-            .toArray();
-
-        expect(sortedDetails[0]).property('count').to.be.equal(67);
-        expect(sortedDetails[1]).property('count').to.be.equal(13);
-        expect(sortedDetails[2]).property('count').to.be.equal(86);
+    it('average', () => {
+        expect(orders.asQueryable().average('o => o.id')).to.equal(3);
     });
 
-    it('should take when id is smaller than 3', () => {
-        const firstTwo = orders.asQueryable().takeWhile('o => o.id < 3').toArray();
-
-        expect(firstTwo).property('length').to.be.equal(2);
-        expect(firstTwo[0]).property('id').to.be.equal(1);
-        expect(firstTwo[1]).property('id').to.be.equal(2);
+    it('count', () => {
+        expect([1, 2, 3, 4].asQueryable().count('i => i > 2')).to.equal(2);
     });
 
-    it('should take when id is smaller than 3', () => {
-        const firstTwo = orders.asQueryable().takeWhile('o => o.id < 3').toArray();
-
-        expect(firstTwo).property('length').to.be.equal(2);
-        expect(firstTwo[0]).property('id').to.be.equal(1);
-        expect(firstTwo[1]).property('id').to.be.equal(2);
-    });
-
-    it('should skip when id is smaller than 3', () => {
-        const biggerTwo = orders.asQueryable().skipWhile('o => o.id < 3').toArray();
-
-        expect(biggerTwo).property('length').to.be.equal(3);
-        expect(biggerTwo[0]).property('id').to.be.equal(3);
-        expect(biggerTwo[1]).property('id').to.be.equal(4);
-        expect(biggerTwo[2]).property('id').to.be.equal(5);
-    });
-
-    it('should group orders by customer', () => {
-        const prodCat = products
-            .asQueryable()
-            .groupBy('p => p.category', 'g => ({ category: g.key, count: g.length })')
-            .toArray();
-
-        expect(prodCat).property('length').to.equal(3);
-        expect(prodCat[0]).property('count').to.equal(3);
-        expect(prodCat[1]).property('count').to.equal(2);
-        expect(prodCat[2]).property('count').to.equal(4);
-    });
-
-    it('should eleminate recurring items', () => {
+    it('distinct', () => {
         const items = [
             { id: 1, name: 'i1' },
             { id: 2, name: 'i2' },
@@ -95,96 +54,120 @@ describe('Query part tests with strings', () => {
         expect(distItems).property('length').to.equal(3);
     });
 
-    it('should return same sequence for defaultIfEmpty', () => {
+    it('first', () => {
+        const p4 = products[3];
+        expect(products.asQueryable().first('p => p.no === p4.no', { p4 })).to.equal(products[3]);
+        expect(() => products.asQueryable().first('p => p.category === "None"')).to.throw();
+    });
+
+    it('firstOrDefault', () => {
+        expect(products.asQueryable().firstOrDefault('p => p.category === "None"')).to.equal(null);
+    });
+
+    it('groupBy', () => {
+        const prodCat = products
+            .asQueryable()
+            .groupBy('p => p.category', 'g => ({ category: g.key, count: g.length })')
+            .toArray();
+
+        expect(prodCat).property('length').to.equal(3);
+        expect(prodCat[0]).property('count').to.equal(3);
+        expect(prodCat[1]).property('count').to.equal(2);
+        expect(prodCat[2]).property('count').to.equal(4);
+    });
+
+    it('last', () => {
+        const last = products[products.length - 1];
+        expect(products.asQueryable().last('p => p.no === lastNo', { lastNo: last.no })).to.equal(last);
+        expect(() => products.asQueryable().last('p => p.category === "None"')).to.throw();
+    });
+
+    it('lastOrDefault', () => {
+        expect(products.asQueryable().lastOrDefault('p => p.category === "None"')).to.equal(null);
+    });
+
+    it('max', () => {
+        expect(products.asQueryable().max('p => p.no')).to.equal('Prd9');
+    });
+
+    it('min', () => {
+        expect(products.asQueryable().min('p => p.no')).to.equal('Prd1');
+    });
+
+    it('orderBy, orderByDescending, thenBy, thenByDescending', () => {
+        const sortedDetails = orders[4].details.asQueryable()
+            .orderBy('d => d.supplier')
+            .thenByDescending('d => d.count')
+            .toArray();
+
+        expect(sortedDetails[0]).property('count').to.be.equal(67);
+        expect(sortedDetails[1]).property('count').to.be.equal(13);
+        expect(sortedDetails[2]).property('count').to.be.equal(86);
+    });
+
+    it('select', () => {
+        const ids = orders.asQueryable().select(o => o.id).toArray();
+        expect(ids).to.deep.equal([1, 2, 3, 4, 5]);
+
+        const idNo = orders.asQueryable().select('o => ({ id: o.id, no: o.no })').toArray();
+        expect(idNo).to.deep.equal([
+            { id: 1, no: 'Ord1' },
+            { id: 2, no: 'Ord2' },
+            { id: 3, no: 'Ord3' },
+            { id: 4, no: 'Ord4' },
+            { id: 5, no: 'Ord5' }
+        ]);
+    });
+
+    it('selectMany', () => {
+        const details = orders.asQueryable().selectMany('o => o.details').toArray();
+        expect(details.length).to.equal(16);
+    });
+
+    it('single', () => {
+        const p4 = products[3];
+        expect(products.asQueryable().single('p => p.no === p4No', { p4No: p4.no })).to.equal(p4);
+        expect(() => products.asQueryable().single('p => p.category === "None"')).to.throw();
+    });
+
+    it('singleOrDefault', () => {
+        expect(products.asQueryable().singleOrDefault('p => p.category === "None"')).to.equal(null);
+    });
+
+    it('skipWhile', () => {
+        const biggerTwo = orders.asQueryable().skipWhile('o => o.id < 3').toArray();
+
+        expect(biggerTwo).property('length').to.be.equal(3);
+        expect(biggerTwo[0]).property('id').to.be.equal(3);
+        expect(biggerTwo[1]).property('id').to.be.equal(4);
+        expect(biggerTwo[2]).property('id').to.be.equal(5);
+    });
+
+    it('sum', () => {
+        expect(orders.asQueryable().sum('o => o.id')).to.equal(15);
+    });
+
+    it('takeWhile', () => {
+        const firstTwo = orders.asQueryable().takeWhile('o => o.id < 3').toArray();
+
+        expect(firstTwo).property('length').to.be.equal(2);
+        expect(firstTwo[0]).property('id').to.be.equal(1);
+        expect(firstTwo[1]).property('id').to.be.equal(2);
+    });
+
+    it('where', () => {
+        const result = orders.asQueryable().where('c => c.id > 3').toArray();
+        expect(result).property('length').to.equal(2);
+        expect(result[0].id).to.equal(4);
+        expect(result[1].no).to.equal('Ord5');
+    });
+
+    it('defaultIfEmpty', () => {
         const arr = [{ id: 1 }, { id: 2 }];
 
         const defEmp = arr.asQueryable().defaultIfEmpty().toArray();
 
         expect(defEmp).to.not.equal(arr);
         expect(defEmp).to.deep.equal(arr);
-    });
-
-    it('should return first item', () => {
-        const p4 = products[3];
-        expect(products.asQueryable().first('p => p.no === p4.no', { p4 })).to.equal(products[3]);
-    });
-
-    it('should return default for missing first item', () => {
-        expect(products.asQueryable().firstOrDefault('p => p.category === "None"')).to.equal(null);
-    });
-
-    it('should throw error for missing first item', () => {
-        expect(() => products.asQueryable().first('p => p.category === "None"')).to.throw();
-    });
-
-    it('should return last item', () => {
-        const last = products[products.length - 1];
-        expect(products.asQueryable().last('p => p.no === lastNo', { lastNo: last.no })).to.equal(last);
-    });
-
-    it('should return default for missing last item', () => {
-        expect(products.asQueryable().lastOrDefault('p => p.category === "None"')).to.equal(null);
-    });
-
-    it('should throw error for missing last item', () => {
-        expect(() => products.asQueryable().last('p => p.category === "None"')).to.throw();
-    });
-
-    it('should return single item', () => {
-        const p4 = products[3];
-        expect(products.asQueryable().single('p => p.no === p4No', { p4No: p4.no })).to.equal(p4);
-    });
-
-    it('should return default for missing single item', () => {
-        expect(products.asQueryable().singleOrDefault('p => p.category === "None"')).to.equal(null);
-    });
-
-    it('should throw error for missing single item', () => {
-        expect(() => products.asQueryable().single('p => p.category === "None"')).to.throw();
-    });
-
-    it('should return if any item matches the predicate', () => {
-        expect([1, 2, 3, 4].asQueryable().any('i => i > 3')).to.be.true;
-        expect([1, 2, 3, 4].asQueryable().any('i => i > 4')).to.be.false;
-
-        const p5 = products[5];
-        expect(products.asQueryable().any('p => p.name === p5.name', { p5 })).to.be.true;
-        expect(products.asQueryable().any('p => p.name === "None"')).to.be.false;
-    });
-
-    it('should return if all items matches the predicate', () => {
-        expect([1, 2, 3, 4].asQueryable().all('i => i > 0')).to.be.true;
-        expect([1, 2, 3, 4].asQueryable().any('i => i > 4')).to.be.false;
-
-        expect(products.asQueryable().all('p => p.category !== null')).to.be.true;
-        expect(products.asQueryable().all('p => p.name === "None"')).to.be.false;
-    });
-
-    it('should return the count of items that matches the predicate', () => {
-        expect([1, 2, 3, 4].asQueryable().count('i => i > 2')).to.equal(2);
-    });
-
-    it('should return the min value', () => {
-        expect(products.asQueryable().min('p => p.no')).to.equal('Prd1');
-    });
-
-    it('should return the max value', () => {
-        expect(products.asQueryable().max('p => p.no')).to.equal('Prd9');
-    });
-
-    it('should return the sum of values', () => {
-        expect(orders.asQueryable().sum('o => o.id')).to.equal(15);
-    });
-
-    it('should return the average of values', () => {
-        expect(orders.asQueryable().average('o => o.id')).to.equal(3);
-    });
-
-    it('should return the aggregated value', () => {
-        expect([1, 2, 3, 4].asQueryable().aggregate('(seed, value) => seed + value')).to.equal(10);
-        expect([1, 2, 3, 4].asQueryable().aggregate('(seed, value) => seed + value', 32)).to.equal(42);
-
-        const agg = orders.asQueryable().aggregate('(seed, order) => seed + order.id', 69, 'v => v / 2');
-        expect(agg).to.equal(42);
     });
 });
