a855534a17c92b1c4263b6a06b5440c14bf8d160
Umut Özel <umutozel@outlook.com>
2018-10-10 16:39:24 +0300

    fix ajax data copy

--- lib/ajax.ts
+++ lib/ajax.ts
@@ -29,7 +29,7 @@ export function mergeAjaxOptions(o1: AjaxOptions, o2: AjaxOptions): AjaxOptions 
     if (o2 == null) return o1;
     
     return {
-        data: o1.data ? Object.assign({}, o1.data, o2.data) : o2.data,
+        data: o1.data ? (o2.data ? Object.assign({}, o1.data, o2.data) : o1.data) : o2.data,
         headers: o1.headers ? Object.assign({}, o1.headers, o2.headers) : o2.headers,
         method: o2.method || o1.method,
         params: (o1.params || []).concat(o2.params || []),
