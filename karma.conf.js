3aa8124f9313fc92add913b5738c528713fada83
Umut Özel <umutozel@outlook.com>
2019-03-21 09:52:14 +0300

    add karma

---
+++ karma.conf.js
@@ -0,0 +1,36 @@
+const browsers = process.platform === "win32"
+    ? ['ChromeHeadless', 'FirefoxHeadless', 'IE']
+    : ['ChromeHeadless', 'FirefoxHeadless'];
+
+module.exports = function (config) {
+    config.set({
+        frameworks: ["mocha", "karma-typescript"],
+
+        files: [{
+                pattern: "lib/**/*.ts"
+            },
+            {
+                pattern: "test/**/*.ts"
+            }
+        ],
+
+        preprocessors: {
+            "**/*.ts": ["karma-typescript"]
+        },
+
+        karmaTypescriptConfig: {
+            tsconfig: "tsconfig.json",
+            include: [
+                "./index.ts",
+                "./lib/**/*.ts",
+                "./test/**/*.ts"
+            ]
+        },
+    
+        reporters: ["dots", "karma-typescript"],
+
+        browsers,
+
+        singleRun: true
+    });
+};
